96481b2f8becdaaec566f36338f9315fd6a6370f
Ritwik Mukherjee <mritwik369@gmail.com>
2019-12-11 21:22:29 +0530

    yeet

--- scripts/events.js
+++ scripts/events.js
@@ -1,4 +1,22 @@
-window.onscroll = function () { animateEvents() };
+// window.onscroll = function () { animateEvents() };
+// document.addEventListener("scroll", animateEvents);
+
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+setInterval(() => {
+    let section = document.querySelector('#events');
+    if (isInViewport(section)) {
+        animateEvents();
+    }
+}, 100);
 
 function animateEvents() {
 
@@ -7,6 +25,8 @@ function animateEvents() {
     let testDiv3 = document.getElementById("eventDiv3");
     let testDiv4 = document.getElementById("eventDiv4");
     let testDiv5 = document.getElementById("eventDiv5");
+
+    console.log('here');
 
 
     let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0); //view height
