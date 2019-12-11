a4940e80c4fc9d7d3fef8a18da3c1ab241be2657
Ritwik Mukherjee <mritwik369@gmail.com>
2019-12-11 21:26:46 +0530

    yeet

--- scripts/events.js
+++ scripts/events.js
@@ -20,46 +20,46 @@ setInterval(() => {
 
 function animateEvents() {
 
-    let testDiv1 = document.getElementById("eventDiv1");
-    let testDiv2 = document.getElementById("eventDiv2");
-    let testDiv3 = document.getElementById("eventDiv3");
-    let testDiv4 = document.getElementById("eventDiv4");
-    let testDiv5 = document.getElementById("eventDiv5");
+    document.getElementById("eventDiv1").className = "blind-content in origin-top";
+    document.getElementById("eventDiv2").className = "blind-content in origin-top";
+    document.getElementById("eventDiv3").className = "blind-content in origin-top";
+    document.getElementById("eventDiv4").className = "blind-content in origin-top";
+    document.getElementById("eventDiv5").className = "blind-content in origin-top";
 
     console.log('here');
 
-
     let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0); //view height
 
-    if (document.body.scrollTop > testDiv1.offsetTop - h || document.documentElement.scrollTop > testDiv1.offsetTop - h) {
-        document.getElementById("eventDiv1").className = "blind-content in origin-top";
-    } else {
-        document.getElementById("eventDiv1").className = "";
-    }
 
-    if (document.body.scrollTop > testDiv2.offsetTop - h || document.documentElement.scrollTop > testDiv2.offsetTop - h) {
-        document.getElementById("eventDiv2").className = "blind-content in origin-top";
-    } else {
-        document.getElementById("eventDiv2").className = "";
-    }
+    // if (document.body.scrollTop > testDiv1.offsetTop - h || document.documentElement.scrollTop > testDiv1.offsetTop - h) {
+    //     document.getElementById("eventDiv1").className = "blind-content in origin-top";
+    // } else {
+    //     document.getElementById("eventDiv1").className = "";
+    // }
+
+    // if (document.body.scrollTop > testDiv2.offsetTop - h || document.documentElement.scrollTop > testDiv2.offsetTop - h) {
+    //     document.getElementById("eventDiv2").className = "blind-content in origin-top";
+    // } else {
+    //     document.getElementById("eventDiv2").className = "";
+    // }
 
 
-    if (document.body.scrollTop > testDiv3.offsetTop - h || document.documentElement.scrollTop > testDiv3.offsetTop - h) {
-        document.getElementById("eventDiv3").className = "blind-content in origin-top";
-    } else {
-        document.getElementById("eventDiv3").className = "";
-    }
+    // if (document.body.scrollTop > testDiv3.offsetTop - h || document.documentElement.scrollTop > testDiv3.offsetTop - h) {
+    //     document.getElementById("eventDiv3").className = "blind-content in origin-top";
+    // } else {
+    //     document.getElementById("eventDiv3").className = "";
+    // }
 
-    if (document.body.scrollTop > testDiv4.offsetTop - h || document.documentElement.scrollTop > testDiv4.offsetTop - h) {
-        document.getElementById("eventDiv4").className = "blind-content in origin-top";
-    } else {
-        document.getElementById("eventDiv4").className = "";
-    }
+    // if (document.body.scrollTop > testDiv4.offsetTop - h || document.documentElement.scrollTop > testDiv4.offsetTop - h) {
+    //     document.getElementById("eventDiv4").className = "blind-content in origin-top";
+    // } else {
+    //     document.getElementById("eventDiv4").className = "";
+    // }
 
-    if (document.body.scrollTop > testDiv4.offsetTop - h || document.documentElement.scrollTop > testDiv4.offsetTop - h) {
-        document.getElementById("eventDiv5").className = "blind-content in origin-top";
-    } else {
-        document.getElementById("eventDiv5").className = "";
-    }
+    // if (document.body.scrollTop > testDiv4.offsetTop - h || document.documentElement.scrollTop > testDiv4.offsetTop - h) {
+    //     document.getElementById("eventDiv5").className = "blind-content in origin-top";
+    // } else {
+    //     document.getElementById("eventDiv5").className = "";
+    // }
 
 }
